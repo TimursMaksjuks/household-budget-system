da9e3c25496c84b5abcf19a29b8dccc332f0eb5a
Timurs Maksjuks <tmaksjuks@gmail.com>
2026-06-03 18:19:35 +0300

    Added project database models and relationships

---
+++ database/migrations/2026_06_03_145304_create_financial_records_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('financial_records', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', 10, 2);
+            $table->date('date');
+            $table->string('description');
+            $table->enum('record_type', [
+        'income',
+        'expense'
+            ]);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('financial_records');
+    }
+};
